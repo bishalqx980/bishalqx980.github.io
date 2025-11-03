bd44453144694dd9bae9eb55d4e765527bf522bf
Bishal <bishalqx680@gmail.com>
2025-11-04 00:10:45 +0600

    Updated Main Page

--- main.js
+++ main.js
@@ -10,4 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
         btn.style.transform = "translateY(10px)";
         btn.style.animation = `fadeIn 0.3s ease forwards ${i * 0.1}s`;
     });
+
+    // visitCounter
+    let siteVisited = Number(localStorage.getItem("siteVisited") || 0);
+    const isVisitCounted = Boolean(sessionStorage.getItem("isVisitCounted"));
+    const visitCounter = document.getElementById("visitCounter");
+
+    if (!isVisitCounted) {
+        siteVisited++;
+        sessionStorage.setItem("isVisitCounted", true);
+        localStorage.setItem("siteVisited", siteVisited);
+    }
+
+    visitCounter.innerText = "Visit Count: " + siteVisited;
 });
